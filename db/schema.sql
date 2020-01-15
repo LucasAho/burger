af33b3b0b748d916fe4573725e91da9baae54d33
Lucas Aho <aholucas11@gmail.com>
2020-01-15 15:06:08 -0700

    sql files run with mysql terminal

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+
 CREATE DATABASE burgers_db;
+
 
 USE burgers_db;
 
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(15) NOT NULL,
-    devoured BOOLEAN DEFAULT FALSE
+    devoured BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
 )
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Truffle Shroom", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Freedom Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Banzai Burger", 0);
