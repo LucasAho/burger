52c9b563c26c33e7d1a97a28e784271e67d377f0
Lucas Aho <aholucas11@gmail.com>
2020-01-15 16:22:22 -0700

    burger.js crud constructor made

--- models/burger.js
+++ models/burger.js
@@ -2,5 +2,27 @@
 var orm = require("../config/orm.js");
 
 //This file will do cool stuff like call ORM functions, just you wait
+var burger = {
+    all: cb => {
+        orm.all("burgers", res => {
+            cb(res);
+        });
+    },
+    create: (cols, vals, cb) => {
+        orm.create("burgers", cols, vals, res => {
+            cb(res);
+        });
+    },
+    update: (objColVals, condition, cb) => {
+        orm.update("burgers", objColVals, condition, res => {
+            cb(res);
+        });
+    },
+    delete: (condition, cb) => {
+        orm.delete("burgers", condition, res => {
+            cb(res);
+        });
+    }
+}
 
 module.exports = burger;
