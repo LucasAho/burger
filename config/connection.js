817a96b88d2e208da717846f38a885f58dfd0b1a
Lucas Aho <aholucas11@gmail.com>
2020-01-18 13:54:27 -0700

    Heroku jawsdb stuff added

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 var mysql = require("mysql");
 
 //Connection to mysql 
-var connection = mysql.createConnection({
-    host:"localhost",
-    port: 3306,
-    user: "root",
-    password: "gitsql",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host:"localhost",
+        port: 3306,
+        user: "root",
+        password: "gitsql",
+        database: "burgers_db"
+    });
+}
 
 //Starts init function with connection
 connection.connect(err => {
